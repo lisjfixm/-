2c331dbabe726879b56aa7a83b59bb56141a7fae
lisjfixm <nneeiillllii.com@gmail.com>
2026-09-26 15:21:35 +0800

    Enable file access from URLs + universal access for WebView

--- android-template/app/src/main/java/com/mrtgd/game/MainActivity.java
+++ android-template/app/src/main/java/com/mrtgd/game/MainActivity.java
@@ -34,6 +34,9 @@ public class MainActivity extends Activity {
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setDatabaseEnabled(true);
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
         settings.setBuiltInZoomControls(false);
